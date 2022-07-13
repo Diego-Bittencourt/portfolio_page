cc27a52502369658da8c3b176e44efd121d538a9
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-07-13 12:39:11 +0900

    INITIAL COMMIT

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Diego Bittencourt Mendes</h1>
+    <ul>
+      <li>Projects</li>
+      <li>Contact</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+li {
+    list-style: none;
+}
+</style>
