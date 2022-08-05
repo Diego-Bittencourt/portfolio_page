183788212714d3d59c64cbb405ad66e1e3304ad6
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-08-05 13:41:55 +0900

    CHORE - Add "bubbling" movement to the slime.

--- src/components/layout/TheSlime.vue
+++ src/components/layout/TheSlime.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="slimewrapper" :style="moveSlime">
-    <div class="slimeoutlayer">
+    {{ floatTime }}
+    <div class="slimeoutlayer" :style="slimeFloat">
       <div class="slimesublayer">
         <div class="slimeinnerlayer" :class="color">
           <div class="eyeswrapper">
-          <div class="left eye" :class="lefteyeshape"></div>
-          <div class="right eye" :class="righteyeshape"></div>
+            <div class="left eye" :class="lefteyeshape"></div>
+            <div class="right eye" :class="righteyeshape"></div>
           </div>
           <div class="slimefoot"></div>
           <div class="whitedot dot1"></div>
@@ -18,63 +19,82 @@
 
 <script>
 export default {
-  inject: ['color'],
+  inject: ["color"],
   data() {
     return {
       slimeColor: "green",
       color: this.color,
-      positionX: 300,
-      positionY: 300,
-      slimeDirection: 1
+      positionX: 45,
+      positionY: 45,
+      slimeDirection: 1,
+      floatTime: "4",
     };
   },
   mounted() {
     //adding a Vanilla JS event listener to the window.
-    window.addEventListener('keypress', this.handleKeyPress);
+    window.addEventListener("keypress", this.handleKeyPress);
+    window.addEventListener("keydown", this.fastFloatTime);
+    window.addEventListener("keyup", this.slowFloatTime);
+    
   },
   methods: {
+    fastFloatTime () {
+      this.floatTime = "0.5"
+    },
+    slowFloatTime () {
+      this.floatTime = "4"
+    },
     handleKeyPress(e) {
       const keyCode = String(e.keyCode || e.code || e.keyIdentifier);
       if (keyCode == "119") {
-        this.positionY = this.positionY - 5;
+        this.positionY = this.positionY - 0.5;
       } else if (keyCode === "97") {
-        this.positionX = this.positionX - 5;
+        this.positionX = this.positionX - 0.5;
         this.slimeDirection = -1;
       } else if (keyCode === "115") {
-        this.positionY = this.positionY + 5;
+        this.positionY = this.positionY + 0.5;
       } else if (keyCode === "100") {
-        this.positionX = this.positionX + 5;
+        this.positionX = this.positionX + 0.5;
         this.slimeDirection = 1;
       }
-
-      
-      console.log(keyCode);
-
     },
   },
   computed: {
-    lefteyeshape () {
+    lefteyeshape() {
       if (this.color.value === "red") {
         return "leftredeyes";
       } else if (this.color.value === "blue") {
         return "leftblueeyes";
-      }  else {
+      } else {
         return "leftgreeneyes";
       }
     },
-    righteyeshape () {
+    righteyeshape() {
       if (this.color.value === "red") {
         return "rightredeyes";
       } else if (this.color.value === "blue") {
         return "rightblueeyes";
-      }  else {
+      } else {
         return "rightgreeneyes";
       }
     },
     moveSlime() {
-      return "left: " + this.positionX + "px; top: " + this.positionY + "px; transform: scaleX(" + this.slimeDirection + ");"
+      return (
+        "left: " +
+        this.positionX +
+        "%; top: " +
+        this.positionY +
+        "%; transform: scaleX(" +
+        this.slimeDirection +
+        ");"
+      );
+    },
+    slimeFloat() {
+      
+        return ("animation-duration: " + this.floatTime + "s;")
+      
     }
-  }
+  },
 };
 </script>
 
@@ -82,18 +102,49 @@ export default {
 .slimewrapper {
   position: absolute;
   width: 100px;
-  height: 75px;
+  height: 65px;
   left: 50%;
   top: 50%;
   z-index: 5;
   transition: 0.1s;
+  border: 1px solid purple;
+}
+
+.slimefloorlayer {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 
 .slimeoutlayer {
   border: 3px solid rgba(0, 0, 0, 0.712);
   border-radius: 100px 100px 100px 50px;
-  width: 100%;
   height: 100%;
+  min-width: 80%;
+  animation: float 6s ease-in-out infinite;
+  /* position: absolute;
+  bottom: 0;
+  left: 0; */
+}
+
+@keyframes float {
+  0% {
+    transform: scaleX(1);
+    width: 90%;
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: scaleY(0.8);
+    width: 100%;
+    transform: translateY(3%);
+  }
+
+  100% {
+    transform: scaleY(1);
+    width: 90%;
+    transform: translateY(0);
+  }
 }
 
 .slimesublayer {
@@ -168,7 +219,6 @@ export default {
   margin-left: -20px;
   display: flex;
   justify-content: space-around;
-
 }
 
 @keyframes blink {
@@ -221,6 +271,5 @@ export default {
   left: 8%;
   background-color: rgba(48, 48, 48, 0.644);
   border-radius: 100%;
-
 }
 </style>
